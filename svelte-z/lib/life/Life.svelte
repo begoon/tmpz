7025161e937e6ce85dc015135be95d11cfcfea30
Alexander Demin <ademin@gmail.com>
2023-04-14 22:59:19 +0100

    Add life implementation

--- svelte-z/lib/life/Life.svelte
+++ svelte-z/lib/life/Life.svelte
@@ -1,4 +1,62 @@
 <script>
+    import { onDestroy } from "svelte";
+
+    let last_time = window.performance.now();
+    let frame;
+
+    let field = [];
+
+    (function update() {
+        frame = requestAnimationFrame(update);
+
+        const time = window.performance.now();
+        const elapsed = time - last_time;
+
+        if (elapsed < 1000) return;
+        last_time = time;
+
+        field = field.map((row, i) =>
+            row.map((cell, j) => {
+                const neighbors = [
+                    field[i - 1]?.[j - 1],
+                    field[i - 1]?.[j],
+                    field[i - 1]?.[j + 1],
+                    field[i][j - 1],
+                    field[i][j + 1],
+                    field[i + 1]?.[j - 1],
+                    field[i + 1]?.[j],
+                    field[i + 1]?.[j + 1],
+                ].filter(Boolean).length;
+                return neighbors === 3 || (cell && neighbors === 2);
+            })
+        );
+    })();
+
+    onDestroy(() => {
+        cancelAnimationFrame(frame);
+    });
+
+    function randomize() {
+        field = Array.from({ length: 150 }, () =>
+            Array.from({ length: 150 }, () => Math.random() < 0.3)
+        );
+    }
+
+    randomize();
 </script>
 
-<div class="text-8xl p-4">I'm LIFE!</div>
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div class="grid h-screen place-items-center" on:click={randomize}>
+    <table>
+        {#each field as row, i}
+            <tr>
+                {#each row as cell, j}
+                    <td
+                        class="p-0 m-0 w-[4px] h-[4px]"
+                        style="background: {cell ? 'green' : 'white'}"
+                    />
+                {/each}
+            </tr>
+        {/each}
+    </table>
+</div>
