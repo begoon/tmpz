0722a136ba5521a9efd958c94623f1cb04e43b40
Alexander Demin <ademin@gmail.com>
2023-04-22 22:18:21 +0100

    Add typescript

--- svelte-z/lib/Root.svelte
+++ svelte-z/lib/Root.svelte
@@ -1,4 +1,13 @@
-<script>
+<script lang="ts">
+    let n: number = 1;
+    let s: string;
+    $: s = n.toString();
+    function f(a: boolean): string {
+        return "f(): " + a;
+    }
 </script>
 
 <div class="text-8xl">I'm ROOT!!!</div>
+<div class="text-8xl">n = {n}</div>
+<div class="text-8xl">s = {s}</div>
+<div class="text-8xl">f() = {f(false)}</div>
--- svelte-z/vite.config.js
+++ svelte-z/vite.config.js
@@ -1,11 +1,12 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { resolve } from "path";
+import sveltePreprocess from "svelte-preprocess";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     root: "lib",
-    plugins: [svelte()],
+    plugins: [svelte({ preprocess: sveltePreprocess() })],
     build: {
         outDir: "../dist",
         emptyOutDir: true,
