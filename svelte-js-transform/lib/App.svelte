47dcd1bcf8068d6724ad3cf61d59249329032907
Alexander Demin <ademin@gmail.com>
2023-06-26 19:28:53 +0100

    Use grid layout

--- svelte-js-transform/lib/App.svelte
+++ svelte-js-transform/lib/App.svelte
@@ -27,35 +27,32 @@
 </script>
 
 <div class="main">
-    <div class="inner">
-        <div style="height: 100%; border: 1px solid brown">
-            <input
-                type="range"
-                bind:value={angleY}
-                min="0"
-                max="180"
-                step="1"
-                orient="vertical"
-            />
-        </div>
-        <div
-            class="scene"
-            on:pointerdown={down}
-            on:pointerup={up}
-            on:pointermove={move}
-        >
-            <div class="text" style="--angleX: {angleX}; --angleY: {angleY}">
-                <svg
-                    viewBox="0 0 58 10"
-                    width="100%"
-                    height="100%"
-                    style="font-size: 10px;"
-                >
-                    <text y="8">Alexander</text>
-                </svg>
-            </div>
+    <input
+        type="range"
+        bind:value={angleY}
+        min="0"
+        max="180"
+        step="1"
+        orient="vertical"
+    />
+    <div
+        class="scene"
+        on:pointerdown={down}
+        on:pointerup={up}
+        on:pointermove={move}
+    >
+        <div class="rotor" style="--angleX: {angleX}; --angleY: {angleY}">
+            <svg
+                viewBox="0 0 58 10"
+                width="100%"
+                height="100%"
+                style="font-size: 10px;"
+            >
+                <text y="8">Alexander</text>
+            </svg>
         </div>
     </div>
+    <div />
     <input type="range" bind:value={angleX} min="0" max="180" step="1" />
 </div>
 
@@ -70,37 +67,25 @@
         overflow: hidden;
     }
     div.main {
-        height: 100vh;
-        border: 1px solid red;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
+        height: 100dvh;
+        border: 0px solid red;
+        display: grid;
+        grid-template-columns: auto 1fr;
+        grid-template-rows: 1fr auto;
         user-select: none;
-    }
-    div.inner {
-        width: 100%;
-        height: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
     }
     div.scene {
         perspective: 1000px;
-        width: 100%;
-        height: 100%;
-        border: 1px solid green;
+        border: 0px solid green;
         display: flex;
-        justify-content: center;
-        align-items: center;
+        padding: 1em;
     }
-    div.text {
+    div.rotor {
         transform: rotateX(calc(var(--angleY) * 1deg))
             rotateY(calc(var(--angleX) * 1deg));
-        max-width: 90vw;
         width: 100%;
-        height: 100%;
-        border: 1px solid blue;
+        border: 1px solid hsl(0, 0%, 0%, 0.5);
+        border-radius: 0.5em;
     }
     svg {
         border: 0px solid yellow;
