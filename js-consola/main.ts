1c5fb580e936a34eabdff527a6a642d5566ebf76
Alexander Demin <ademin@gmail.com>
2023-11-22 09:03:34 +0100

    More tests for consola

--- js-consola/main.ts
+++ js-consola/main.ts
@@ -1,6 +1,6 @@
-import { consola } from "npm:consola";
+import process from "node:process";
 
-Date.prototype.toLocaleTimeString = Date.prototype.toISOString;
+import { consola } from "npm:consola";
 
 const kinds = [
     "silent",
@@ -15,10 +15,26 @@ const kinds = [
     "start",
     "box",
     "debug",
-    "trace",
     "verbose",
+    // "trace",
 ] as const;
 
+Date.prototype.toLocaleTimeString = Date.prototype.toISOString;
+consola.options.formatOptions.compact = false;
+consola.log("default level", consola.level); // 3
+if (process.argv.includes("--verbose")) consola.level = 9;
 kinds.forEach((kind) => {
-    consola[kind]("hello world");
+    consola[kind]("hello world", kind);
+});
+
+consola.box("exception", "box", {
+    number: 1,
+    boolean: true,
+    object: { a: 1 },
+    array: [1, 2, 3],
+    date: new Date(),
+    // error: new Error("hello world"),
+    symbol: Symbol("symbol"),
+    null: null,
+    undefined: undefined,
 });
