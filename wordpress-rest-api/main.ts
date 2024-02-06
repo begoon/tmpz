90e82256c8b657440733006c0aa840be09c6390d
Alexander Demin <ademin@gmail.com>
2024-02-06 16:10:28 +0000

    Add wordpress-rest-api

---
+++ wordpress-rest-api/main.ts
@@ -0,0 +1,68 @@
+import { encodeBase64 } from "https://deno.land/std@0.214.0/encoding/base64.ts";
+import process from "node:process";
+
+const env = process.env;
+const { WP_API, WP_API_USER, WP_API_PASSWORD } = env;
+
+const endpoint = WP_API + "/wc/v3/customers";
+const auth = "Basic " + encodeBase64(WP_API_USER + ":" + WP_API_PASSWORD);
+
+async function print(str: string) {
+    await Deno.stdout.write(new TextEncoder().encode(str));
+}
+
+const needle = process.argv[2];
+if (!needle) {
+    console.error("please provide a username to search for.");
+    process.exit(1);
+}
+
+const url = `${endpoint}?search=${needle}&role=administrator`;
+console.log(url);
+
+await print(`\r\x1b[2Ksearching for "${needle}"... `);
+const response = await fetch(url, {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json",
+        Authorization: auth,
+    },
+});
+
+const data = (await response.json()) as {
+    id: number;
+    username: string;
+    first_name: string;
+    last_name: string;
+    email: string;
+}[];
+
+if (!response.ok) {
+    console.error(response.statusText, data);
+    process.exit(1);
+}
+
+if (!data.length) {
+    console.error("not found");
+    process.exit(0);
+}
+
+const { id, username, first_name, last_name, email } = data[0];
+console.log(id, username, first_name, last_name, email);
+
+const password = process.argv[3];
+if (password) {
+    console.log("logging in...");
+    const api = env.FREEMYND_API;
+    const url = `${api}/user/${username}/auth`;
+    const response = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ user_pass: password }),
+    });
+    if (!response.ok) {
+        console.error(response.statusText);
+        process.exit(1);
+    }
+    console.log(await response.json());
+}
