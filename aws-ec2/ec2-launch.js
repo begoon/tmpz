a6652b2f703873fc02cd921176e39a6839dd69bc
Alexander Demin <ademin@gmail.com>
2024-05-19 15:01:47 +0100

    Use SDK v3

--- aws-ec2/ec2-launch.js
+++ aws-ec2/ec2-launch.js
@@ -1,52 +1,46 @@
-import AWS from "aws-sdk";
-
-// https://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_RunInstances.html
-
-AWS.config.update({ region: "eu-west-2" });
+import { EC2 } from "@aws-sdk/client-ec2";
 
 const cmd = process.argv[2];
 
-const ec2 = new AWS.EC2({ apiVersion: "2016-11-15" });
+const client = new EC2({ region: "eu-west-2" });
 
 const now = new Date().toISOString();
 
 async function start(tags) {
-    const instance = await ec2
-        .runInstances({
-            ImageId: "ami-01f10c2d6bce70d90",
-            InstanceType: "t2.micro",
-            MinCount: 1,
-            MaxCount: 1,
-            UserData: Buffer.from(
-                [
-                    "#!/bin/bash",
-                    "sudo yum update -y",
-                    "sudo yum install docker -y",
-                    "sudo systemctl start docker",
-                    `sudo echo "webserver started at ${now}" >index.html`,
-                    "sudo docker run -d -v $PWD/index.html:/usr/share/caddy/index.html -p :80:80 caddy",
-                ].join("\n")
-            ).toString("base64"),
-            SecurityGroupIds: ["sg-046938788a0c5da74"],
-        })
-        .promise();
+    const instance = await client.runInstances({
+        ImageId: "ami-01f10c2d6bce70d90",
+        InstanceType: "t2.micro",
+        MinCount: 1,
+        MaxCount: 1,
+        UserData: Buffer.from(
+            [
+                "#!/bin/bash",
+                "sudo yum update -y",
+                "sudo yum install docker -y",
+                "sudo systemctl start docker",
+                `sudo echo "webserver started at ${now}" >index.html`,
+                "sudo docker run -d -v $PWD/index.html:/usr/share/caddy/index.html -p :80:80 caddy",
+            ].join("\n")
+        ).toString("base64"),
+        SecurityGroupIds: ["sg-046938788a0c5da74"],
+    });
     var instanceId = instance.Instances[0].InstanceId;
     console.log("created instance", instanceId);
 
-    await ec2.createTags({ Resources: [instanceId], Tags: tags }).promise();
+    await client.createTags({ Resources: [instanceId], Tags: tags });
     console.log(`instance ${instanceId} tagged`);
     return instanceId;
 }
 
 async function terminate(instanceId) {
-    await ec2.terminateInstances({ InstanceIds: [instanceId] }).promise();
+    await client.terminateInstances({ InstanceIds: [instanceId] });
     console.log(`instance ${instanceId} terminated`);
 }
 
 async function state(instanceId) {
-    const instance = await ec2
-        .describeInstances({ InstanceIds: [instanceId] })
-        .promise();
+    const instance = await client.describeInstances({
+        InstanceIds: [instanceId],
+    });
     return {
         state: instance.Reservations[0].Instances[0].State.Name,
         ip: instance.Reservations[0].Instances[0].PublicIpAddress,
@@ -58,7 +52,9 @@ async function awaitState(instanceId, desiredState) {
     while (true) {
         const { state: current, ip } = await state(instanceId);
         process.stdout.write(
-            `\rwaiting ${instanceId} ${ip} /${current} to be ${desiredState} / ${seconds}s`
+            `\rwaiting ${instanceId} ${
+                ip || "-"
+            } (${current}) to be ${desiredState} / ${seconds}s`
         );
         seconds += 1;
         if (current === desiredState) break;
@@ -74,9 +70,9 @@ if (cmd === "start") {
         { Key: "Owner", Value: process.env.USER },
     ];
     const instanceId = await start(tags);
-    await awaitState(instanceId, "running").then(() => {
-        console.log(`instance ${instanceId} is running`);
-    });
+    await awaitState(instanceId, "running");
+    console.log(`instance ${instanceId} is running`);
+
     const ended = performance.now();
     console.log(`time taken: ${ended - started}ms`);
 }
