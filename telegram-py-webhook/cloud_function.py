85fcc808c0f1f674052164afa61e228c3321e70f
Alexander Demin <ademin@gmail.com>
2024-05-28 20:58:30 +0100

    Add status code in flask

--- telegram-py-webhook/cloud_function.py
+++ telegram-py-webhook/cloud_function.py
@@ -1,6 +1,6 @@
-import json
 import os
 
+import flask
 import functions_framework
 
 import bot_sync as bot
@@ -13,7 +13,7 @@ bot.starter(wh)
 
 
 @functions_framework.http
-def function_handler(request):
+def function_handler(request: flask.Request):
     method = request.method
     path = request.path
 
@@ -22,9 +22,10 @@ def function_handler(request):
 
     if method == "POST" and path == "/bot":
         request_json = request.get_json(silent=True)
+        if request_json is None:
+            return {"error": "invalid json"}
         print(request_json)
         bot.update(request_json)
         return {}
 
-    return 'Ha?'
-    return 'Ha?'
+    return ('Ha?', 418)
