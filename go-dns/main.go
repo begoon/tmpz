2fa62d3067ca957a9e0ab559710cd2682f9890ed
Alexander Demin <ademin@gmail.com>
2024-06-28 18:49:52 +0100

    More goodies

--- go-dns/main.go
+++ go-dns/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"log/slog"
 	"net/http"
 	"os"
 	"time"
@@ -13,17 +14,40 @@ import (
 	"cloud.google.com/go/storage"
 )
 
+var SHA string
+
 func main() {
+	h := slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			if a.Key == slog.LevelKey {
+				a.Key = "severity"
+				return a
+			}
+			if a.Key == slog.MessageKey {
+				a.Key = "message"
+				return a
+			}
+			return a
+		},
+	})
+	slog.SetDefault(slog.New(h))
+
 	debug := os.Getenv("GODEBUG")
 	fmt.Printf("GODEBUG=[%s]\n", debug)
 	if debug == "" {
 		os.Setenv("GODEBUG", "netdns=2")
 		fmt.Printf("FORCED GODEBUG=[%s]\n", os.Getenv("GODEBUG"))
 	}
+	fmt.Printf("SHA=%v\n", SHA)
 
-	http.HandleFunc("GET /x/{path...}", server)
+	http.HandleFunc("GET /z/{path...}", server)
 	http.HandleFunc("GET /env", env)
+	http.HandleFunc("GET /error", boom)
+	http.HandleFunc("POST /action", action)
+
 	fmt.Println("listening on :8000")
+	slog.Info("listening on :8000")
 	http.ListenAndServe(":8000", nil)
 }
 
@@ -33,8 +57,34 @@ func server(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
 	fmt.Printf("GODEBUG=[%s] method=%s path=%s\n", debug, r.Method, path)
 	ip := myip()
-	fmt.Fprintf(w, "stored [%s] [%s] [%s] $=[%s]\n", path, debug, ip, v)
+	fmt.Fprintf(w, "stored [%s] [%s] [%s] commit=%v $=[%s]\n", path, debug, ip, SHA, v)
 	store(path)
+	slog.Info("REQUEST", "path", path, "ip", ip, "v", v)
+}
+
+func action(w http.ResponseWriter, r *http.Request) {
+	var v map[string]interface{}
+	data, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "error reading body: %v", err)
+		return
+	}
+	if err := json.Unmarshal(data, &v); err != nil {
+		fmt.Fprintf(w, "error unmarshalling: %v", err)
+		return
+	}
+	v["status"] = "alive"
+	vv, err := json.MarshalIndent(v, "", "  ")
+	fmt.Fprintf(w, string(vv))
+	if err != nil {
+		fmt.Fprintf(w, "error marshalling: %v", err)
+		return
+	}
+	slog.Info("ACTION", "v", v)
+}
+
+func boom(w http.ResponseWriter, r *http.Request) {
+	slog.Error("BOOM", "path", r.URL.Path, "reason", r.Header.Get("X-Forwarded-For"))
 }
 
 func env(w http.ResponseWriter, r *http.Request) {
