94f0ef271ce383e3ff7b587197c16311cc5ad6d4
Alexander Demin <ademin@gmail.com>
2024-07-10 20:35:54 +0200

    Add gcp-firestore

---
+++ gcp-firestore/main.ts
@@ -0,0 +1,24 @@
+import { Firestore } from "@google-cloud/firestore";
+import { GoogleAuth } from "google-auth-library";
+
+import { Buffer } from "node:buffer";
+import { env } from "node:process";
+
+const key = Buffer.from(env.GCLOUND_CREDENTIALS_BASE64!, "base64").toString();
+const credentials = JSON.parse(key);
+
+const auth = new GoogleAuth({ credentials: credentials });
+
+const firestore = new Firestore({ authClient: await auth.getClient() });
+
+const document = firestore.doc("karta/wheel");
+
+await document.set({
+    title: "Welcome to Firestore",
+    body: "Hello World",
+});
+
+const read = await document.get();
+console.log("read", read.data());
+
+await document.delete();
