7360c203601dfa3e8892b1e14edb2bf9a9aaa268
Alexander Demin <ademin@gmail.com>
2024-07-16 11:47:14 +0200

    Update js-consola

--- js-consola/main.ts
+++ js-consola/main.ts
@@ -1,8 +1,8 @@
 import process from "node:process";
 const argv = process.argv;
 
-import { LogType, consola } from "npm:consola";
-import { colors } from "npm:consola/utils";
+import { LogType, consola } from "consola";
+import { colors } from "consola/utils";
 
 const kinds = [
     "silent",
@@ -22,7 +22,8 @@ const kinds = [
 ] as const;
 
 Date.prototype.toLocaleTimeString = Date.prototype.toISOString;
-consola.options.formatOptions.compact = false;
+consola.options.formatOptions.compact = !false;
+consola.options.formatOptions.columns = 1;
 
 consola.log("default level", consola.level); // 3
 
@@ -30,10 +31,14 @@ if (argv.includes("--verbose"))
     consola.level = consola.options.types.verbose.level!;
 
 const i = argv.indexOf("--loglevel");
-if (i && argv[i + 1])
-    consola.level = consola.options.types[argv[i + 1] as LogType]?.level || 0;
+if (i && argv[i + 1]) {
+    const level = argv[i + 1] as LogType;
+    process.env.CONSOLA_LEVEL = level;
+}
 
-console.log("types", consola.options.types);
+const error = argv.includes("--error");
+
+// console.log("types", consola.options.types);
 
 kinds.forEach((kind) => {
     consola[kind]("hello world", colors.redBright(kind));
@@ -45,7 +50,7 @@ consola.box("exception", "box", {
     object: { a: 1 },
     array: [1, 2, 3],
     date: new Date(),
-    // error: new Error("hello world"),
+    error: new Error("hello world"),
     symbol: Symbol("symbol"),
     null: null,
     undefined: undefined,
