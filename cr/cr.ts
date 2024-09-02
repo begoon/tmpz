ba1bf6c42c368d20a3a9cba7d57321eea4733eb8
Alexander Demin <ademin@gmail.com>
2024-09-02 14:08:04 +0200

    Build to binary

--- cr/cr.ts
+++ cr/cr.ts
@@ -4,25 +4,10 @@ import consola, { LogLevels } from "consola";
 import fs from "node:fs";
 
 consola.options.formatOptions.columns = 0;
-
-function flag(name: string) {
-    const i = process.argv.findIndex((v) => v === name);
-    if (i === -1) return undefined;
-    process.argv.splice(i, 1);
-    return true;
-}
-
-function option(name: string, default_: string | undefined = undefined) {
-    const i = process.argv.findIndex((v) => v === name);
-    if (i === -1) return default_;
-    const value = process.argv[i + 1];
-    if (!value) return default_;
-    process.argv.splice(i, 2);
-    return value;
-}
+consola.options.formatOptions.compact = false;
 
 const verbose = flag("--verbose");
-if (verbose) consola.level = LogLevels.debug;
+consola.level = verbose ? LogLevels.debug : LogLevels.info;
 
 const MakefileName = option("-f", "Makefile");
 if (!MakefileName || !fs.existsSync(MakefileName)) {
@@ -33,31 +18,7 @@ consola.info(MakefileName);
 
 const Makefile = fs.readFileSync(MakefileName, "utf-8");
 
-function parseMakefile(content: string) {
-    const lines = content.split("\n");
-    const variables = {};
-    for (const line of lines) {
-        if (line.startsWith("#")) continue;
-        const [name, value] = line.split("=");
-        if (value && name.toUpperCase().trim() === name)
-            variables[name] = value;
-    }
-    consola.debug("variables", { variables });
-    return {
-        variables,
-        get: (name: string, echo = false) => {
-            const v = variables[name];
-            if (!v) {
-                consola.error(`variable ${name} not found`);
-                process.exit(1);
-            }
-            if (echo) consola.info(name, "=", v);
-            return v;
-        },
-    };
-}
-
-const makefile = parseMakefile(Makefile);
+const makefile = parseVariables(Makefile);
 
 const NAME = makefile.get("NAME");
 const REPO = makefile.get("REPO");
@@ -66,45 +27,44 @@ const REGION = makefile.get("REGION");
 const PROJECT = makefile.get("PROJECT");
 const SERVICE_NAME = makefile.get("SERVICE_NAME");
 
-const cmd = process.argv[2];
+const service = await info();
+consola.info("image", service.image);
 
-async function info() {
-    consola.info("query", { NAME, REPO, REGION, PROJECT, SERVICE_NAME });
-    const data =
-        await $`gcloud run services describe ${SERVICE_NAME} --region ${REGION} --project ${PROJECT} --format json`.json();
-    consola.debug("info", JSON.stringify(data, null, 2));
-    return {
-        url: data.status.url,
-        image: data.spec.template.spec.containers[0].image,
-    };
+const commands = process.argv.slice(2);
+
+if (commands.length > 0) {
+    consola.debug("commands", commands);
+    for (const command of commands) {
+        consola.debug("command", command);
+        switch (command) {
+            case "info":
+            case "i":
+                consola.info(service);
+                break;
+            case "tags":
+            case "t":
+                consola.info(await tags());
+                break;
+            case "tag":
+            case "l":
+                consola.info((await tags())[0]);
+                break;
+            case "health":
+            case "h":
+                consola.info(await health());
+                break;
+            default:
+                consola.error("ha?", command);
+                process.exit(1);
+        }
+    }
+    process.exit(0);
 }
 
-async function tags() {
-    const data =
-        await $`gcloud artifacts docker images list ${REPO}/${NAME} --include-tags --sort-by "~UPDATE_TIME" --limit 10 --format json`.json();
-    consola.debug("tags", JSON.stringify(data, null, 2));
-    return data
-        .map((image: { tags: string[] }) => image.tags)
-        .reduce((a: string[], v: string) => a.concat(v), [])
-        .filter((tag: string) => tag !== "latest");
-}
-
-const { url, image } = await info();
-
-consola.info("image", image);
-
-consola.info("dial", url + "/health");
-
-const health = async () => await (await fetch(`${url}/health`)).json();
-
+consola.info("dial", service.url + "/health");
 consola.info("health", await health());
 
 const options = await tags();
-
-if (flag("--last")) {
-    consola.box(options[0]);
-    process.exit(0);
-}
 
 const update = await consola.prompt("deploy?", {
     type: "select",
@@ -125,3 +85,65 @@ for await (let line of $`gcloud run deploy ${SERVICE_NAME} --region ${REGION} --
 consola.info("OK");
 
 consola.info(await health());
+
+function flag(name: string) {
+    const i = process.argv.findIndex((v) => v === name);
+    if (i === -1) return undefined;
+    process.argv.splice(i, 1);
+    return true;
+}
+
+function option(name: string, default_: string | undefined = undefined) {
+    const i = process.argv.findIndex((v) => v === name);
+    if (i === -1) return default_;
+    const value = process.argv[i + 1];
+    if (!value) return default_;
+    process.argv.splice(i, 2);
+    return value;
+}
+
+function parseVariables(content: string) {
+    const lines = content.split("\n");
+    const values = {};
+    for (const line of lines) {
+        if (line.startsWith("#")) continue;
+        const [name, value] = line.split("=");
+        if (value && name.toUpperCase().trim() === name) values[name] = value;
+    }
+    consola.debug("variables", { values });
+    return {
+        get: (name: string) => {
+            const v = values[name];
+            if (!v) {
+                consola.error("variable not found", name);
+                process.exit(1);
+            } else consola.debug(name, "=", v);
+            return v;
+        },
+    };
+}
+
+async function info() {
+    consola.info("query", { NAME, REPO, REGION, PROJECT, SERVICE_NAME });
+    const data =
+        await $`gcloud run services describe ${SERVICE_NAME} --region ${REGION} --project ${PROJECT} --format json`.json();
+    consola.debug("info", JSON.stringify(data, null, 2));
+    return {
+        url: data.status.url,
+        image: data.spec.template.spec.containers[0].image,
+    };
+}
+
+async function health() {
+    return await (await fetch(`${service.url}/health`)).json();
+}
+
+async function tags() {
+    const data =
+        await $`gcloud artifacts docker images list ${REPO}/${NAME} --include-tags --sort-by "~UPDATE_TIME" --limit 10 --format json`.json();
+    consola.debug("tags", JSON.stringify(data, null, 2));
+    return data
+        .map((image: { tags: string[] }) => image.tags)
+        .reduce((a: string[], v: string) => a.concat(v), [])
+        .filter((tag: string) => tag !== "latest");
+}
