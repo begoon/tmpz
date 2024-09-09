80050dc11eef195e84b0a43dd28d3761349ad552
Alexander Demin <ademin@gmail.com>
2024-09-09 15:28:13 +0200

    Add puppeteer

---
+++ puppeteer/index.ts
@@ -0,0 +1,8 @@
+import puppeteer from "puppeteer";
+
+const browser = await puppeteer.launch();
+const page = await browser.newPage();
+await page.goto("http://localhost:8000");
+await page.pdf({ path: "example.pdf", format: "A4" });
+
+await browser.close();
