9b1d95ff521a4da5174867105dd7c79b0eeb651f
Alexander Demin <ademin@gmail.com>
2024-09-19 08:38:26 +0200

    Update

--- openai/openai.ts
+++ openai/openai.ts
@@ -8,7 +8,7 @@ const openai = new OpenAI({
 
 const chatCompletion = await openai.chat.completions.create({
     model: "gpt-3.5-turbo-instruct",
-    prompt: "i want to diei want to diei want to die",
+    prompt: "can gemini generate images from text?",
     temperature: 1,
     max_tokens: 2048,
     top_p: 1,
