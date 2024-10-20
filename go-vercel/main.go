f970aac6c7563f4c04244fc7ddad602c18693ff7
Alexander Demin <ademin@gmail.com>
2024-10-20 02:37:35 +0100

    Add tailwindcss

--- go-vercel/main.go
+++ go-vercel/main.go
@@ -6,6 +6,8 @@ import (
 	"go-vercel/api"
 )
 
+//go:generate tailwindcss -i api/style.css -o api/html/style.css
+
 func main() {
 	http.HandleFunc("/", api.Handler)
 	http.ListenAndServe(":8000", nil)
