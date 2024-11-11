1e9d76085c1122bb89d8f041324f37dc49eceb46
Alexander Demin <ademin@gmail.com>
2024-11-11 23:35:08 +0000

    Add errors to ws

--- fastapi-websocket/main.py
+++ fastapi-websocket/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+import traceback
+from fastapi import FastAPI, WebSocketDisconnect
 from starlette.websockets import WebSocket
 
 app = FastAPI()
@@ -16,3 +17,25 @@ async def websocket_endpoint(websocket: WebSocket) -> None:
         if data == "1/0":
             1 / 0
     print(websocket.state)
+
+@app.websocket("/wsx")
+async def websocket_endpoint(websocket: WebSocket) -> None:
+    print("CONNECTED")  
+    await websocket.accept()
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await websocket.send_text(f"message text was: {data}")
+            if data == "close":
+                await websocket.close()
+                break
+            if data == "error":
+                1 / 0
+    except WebSocketDisconnect as e:
+        print(f"WS/SOCKET DISCONNECTED {e.code=} {e.reason=}")
+    except Exception as e:
+        print(f"EXCEPTION {e=} {websocket.client_state=}")
+        await websocket.send_text(f"error: {e}")
+        await websocket.close(code=1011, reason='unhandled websocket exception')
+    finally:
+        print("DISCONNECTED")
