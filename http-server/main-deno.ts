cb569e3ea759dff8bfd21c7e0c94b9c0ad737b51
Alexander Demin <ademin@gmail.com>
2024-11-17 00:57:30 +0000

    Move http-server to a separate repo

--- http-server/main-deno.ts
+++
@@ -1,7 +0,0 @@
-Deno.serve({ port: 8000 }, async (request: Request) => {
-    const pathname = new URL(request.url).pathname;
-    if (request.method === "GET" && pathname === "/version") {
-        return Response.json({ version: "1.0.0" });
-    }
-    return new Response(null, { status: 404 });
-});
--- http-server/main.py
+++
@@ -1,7 +0,0 @@
-import fastapi
-
-application = fastapi.FastAPI()
-
-@application.get("/version", response_model=dict[str, str])
-def version() -> dict[str, str]:
-    return {"version": "1.0.0"}
