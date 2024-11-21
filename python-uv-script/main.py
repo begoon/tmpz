9269dfd2b6ae7a5cce0d2cba153c838cd5457ceb
Alexander Demin <ademin@gmail.com>
2024-11-21 17:34:31 +0000

    Add python-uv-script

---
+++ python-uv-script/main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env -S uv run
+# /// script
+# requires-python = ">=3.12"
+# dependencies = ["fastapi", "httpx", "uvicorn" ]
+# ///
+import asyncio, fastapi, httpx, uvicorn, signal, os
+from typing import Any
+
+application = fastapi.FastAPI()
+
+@application.get("/")
+async def _() -> dict[str, Any]:
+    return { "status": "ha?" }
+
+async def listener():
+    config = uvicorn.Config("main:application", port=8000)
+    server = uvicorn.Server(config)
+    await server.serve()
+
+async def make_request():
+    await asyncio.sleep(0.5)
+    async with httpx.AsyncClient() as client:
+        response = await client.get("http://127.0.0.1:8000/")
+        print(response.json())
+
+async def main():
+    async with asyncio.TaskGroup() as tasks:
+        server = tasks.create_task(listener())
+        client = tasks.create_task(make_request())
+        
+        await asyncio.wait([server, client], return_when=asyncio.FIRST_COMPLETED)
+        os.kill(os.getpid(), signal.SIGTERM)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
