d407ed905e8ec8246b209314b427046073690934
Alexander Demin <ademin@gmail.com>
2024-12-05 15:12:27 +0100

    Use std.http.Client.fetch

--- zig-metadata-ip/main.zig
+++ zig-metadata-ip/main.zig
@@ -3,47 +3,42 @@ const std = @import("std");
 const metaURL = "http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip";
 const ipURL = "https://api.ipify.org";
 
-pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
-
-    var client = std.http.Client{ .allocator = allocator };
-    defer client.deinit();
-
-    var url: []const u8 = undefined;
-    if (std.posix.getenv("X") == null) {
-        url = metaURL;
-    } else {
-        url = ipURL;
-    }
-
-    const uri = try std.Uri.parse(url);
+fn print_external_ip(allocator: std.mem.Allocator, uri: std.Uri) !void {
     const headers = [_]std.http.Header{std.http.Header{
         .name = "Metadata-Flavor",
         .value = "Google",
     }};
 
-    var buf: [4096]u8 = undefined;
-    var request = client.open(.GET, uri, .{
-        .server_header_buffer = &buf,
+    var client = std.http.Client{ .allocator = allocator };
+    defer client.deinit();
+
+    var response_buf = std.ArrayList(u8).init(allocator);
+    defer response_buf.deinit();
+
+    const result = client.fetch(.{
+        .location = .{ .uri = uri },
         .extra_headers = &headers,
+        .response_storage = .{ .dynamic = &response_buf },
     }) catch |err| {
         std.debug.print("{any}: {any}\n", .{ err, uri });
-        return;
+        return err;
     };
-    defer request.deinit();
 
-    try request.send();
-    try request.finish();
-    try request.wait();
-
-    if (request.response.status != .ok) {
-        std.debug.print("error: {d}\n", .{request.response.status});
+    if (result.status != .ok) {
+        std.debug.print("error: {d}\n", .{result.status});
         return;
     }
+    std.debug.print("{s}\n", .{response_buf.items});
+}
 
-    const sz = try request.readAll(&buf);
-    const data = buf[0..sz];
-    std.debug.print("{s}\n", .{data});
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var uri = try std.Uri.parse(metaURL);
+    print_external_ip(allocator, uri) catch {
+        uri = try std.Uri.parse(ipURL);
+        try print_external_ip(allocator, uri);
+    };
 }
