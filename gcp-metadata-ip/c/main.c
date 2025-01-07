0e34b41f3540d3b71e17e9ec0b187ed9bd5163bd
Alexander Demin <ademin@gmail.com>
2025-01-07 19:55:40 +0000

    Reformat

--- gcp-metadata-ip/c/main.c
+++ gcp-metadata-ip/c/main.c
@@ -9,36 +9,43 @@
 
 #define BUFFER_SIZE 4096
 
-void error(const char *msg) {
+void error(const char *msg)
+{
     perror(msg);
     exit(EXIT_FAILURE);
 }
 
-int perform_get(const char *hostname, const char *path, const char *port) {
+int perform_get(const char *hostname, const char *path, const char *port)
+{
     struct addrinfo hints, *result, *rp;
     int s, sockfd;
 
     memset(&hints, 0, sizeof(struct addrinfo));
-    hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+    hints.ai_family = AF_UNSPEC; /* Allow IPv4 or IPv6 */
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = 0;
     hints.ai_protocol = 0;
 
     s = getaddrinfo(hostname, port, &hints, &result);
-    if (s != 0) {
+    if (s != 0)
+    {
         fprintf(stderr, "getaddrinfo: %s: %s\n", gai_strerror(s), hostname);
         return 1;
     }
 
-    for (rp = result; rp != NULL; rp = rp->ai_next) {
+    for (rp = result; rp != NULL; rp = rp->ai_next)
+    {
         sockfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
-        if (sockfd == -1) continue;
+        if (sockfd == -1)
+            continue;
 
-        if (connect(sockfd, rp->ai_addr, rp->ai_addrlen) != -1) break;
+        if (connect(sockfd, rp->ai_addr, rp->ai_addrlen) != -1)
+            break;
         close(sockfd);
     }
 
-    if (rp == NULL) {
+    if (rp == NULL)
+    {
         fprintf(stderr, "error connecting to %s:%s\n", hostname, port);
         return 1;
     }
@@ -51,36 +58,42 @@ int perform_get(const char *hostname, const char *path, const char *port) {
              "Host: %s\r\n"
              "Metadata-Flavor: Google\r\n"
              "Connection: close\r\n"
-            "\r\n",
+             "\r\n",
              path, hostname);
 
-    if (write(sockfd, request, strlen(request)) < 0) error("error writing to socket\n");
+    if (write(sockfd, request, strlen(request)) < 0)
+        error("error writing to socket\n");
 
     char response[BUFFER_SIZE];
     ssize_t n;
 
-    while ((n = read(sockfd, response, BUFFER_SIZE - 1)) > 0) {
+    while ((n = read(sockfd, response, BUFFER_SIZE - 1)) > 0)
+    {
         response[n] = '\0';
         const char *crlf = strstr(response, "\r\n\r\n");
-        if (crlf) {
+        if (crlf)
+        {
             printf("%s", crlf + 4);
             break;
         }
     }
 
-    if (n < 0) error("reading from socket\n");
+    if (n < 0)
+        error("reading from socket\n");
 
     close(sockfd);
     return 0;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     int error;
     error = perform_get("metadata.google.internal", "/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip", "80");
-    if (error) {
+    if (error)
+    {
         error = perform_get("api.ipify.org", "/", "80");
-        if (error) exit(EXIT_FAILURE);
+        if (error)
+            exit(EXIT_FAILURE);
     }
     return 0;
 }
-
