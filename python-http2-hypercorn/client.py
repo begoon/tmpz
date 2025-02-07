9905e061efe31cad6ef86b27e8530d551b87405f
Alexander Demin <ademin@gmail.com>
2025-02-07 19:50:32 +0000

    Add python-http2-hypercorn

---
+++ python-http2-hypercorn/client.py
@@ -0,0 +1,74 @@
+import hashlib
+import io
+import json
+import sys
+import time
+
+import httpx
+import humanize
+
+host = sys.argv[1] if len(sys.argv) > 1 else "http://localhost:8000"
+
+
+ANSI_CLEAR_LINE = "\033[K"
+
+
+class IO(io.BytesIO):
+    def __init__(self, *args, **kwargs):
+        self.n = 0
+        self.i = 0
+        self.started_at = time.monotonic()
+        super().__init__(*args, **kwargs)
+
+    def read(self, size=-1):
+        data = super().read(size)
+        self.n += len(data)
+        print(
+            f"{self.n}: {len(data)=} {self.i}\r",
+            end="",
+            flush=True,
+            file=sys.stderr,
+        )
+        if len(data) == 0:
+            elapsed = time.monotonic() - self.started_at
+            throughput = humanize.naturalsize(self.n / elapsed)
+            print(
+                f"{self.n} bytes read in {elapsed:.2f} seconds "
+                f"({throughput=}){ANSI_CLEAR_LINE}",
+                ANSI_CLEAR_LINE,
+                file=sys.stderr,
+            )
+        self.i += 1
+        return data
+
+
+if __name__ == "__main__":
+    response = httpx.get(host + "/health")
+    response.raise_for_status()
+    print(response.text)
+
+    data = ("0" * 1000 * 1000 * 80).encode()
+    sha1 = hashlib.sha1(data).hexdigest()
+    buf = IO(data)
+
+    started = time.monotonic()
+
+    client = httpx.Client(http2=True)
+    response = client.post(
+        host + "/upload/",
+        files=[
+            (
+                "metadata",
+                (
+                    "metadata",
+                    json.dumps({"metadata": "-metadata-", "sha1": sha1}),
+                    "application/json",
+                ),
+            ),
+            ("raw", ("raw", buf, "application/octet-stream")),
+        ],
+    )
+    elapsed = time.monotonic() - started
+    throughput = humanize.naturalsize(len(data) / elapsed)
+    print(f"{response.status_code=} {throughput=}", file=sys.stderr)
+    print(response.text)
---
+++ python-http2-hypercorn/main.py
@@ -0,0 +1,53 @@
+import hashlib
+import json
+import os
+import time
+
+import humanize
+from fastapi import FastAPI, Request, UploadFile
+from pydantic import BaseModel
+
+application = FastAPI()
+
+
+@application.get("/")
+def index(request: Request):
+    print(request.method, request.url.path)
+    return dict(os.environ)
+
+
+@application.get("/health")
+def health(request: Request):
+    print(request.method, request.url.path)
+    return {"version": "0.0.0"}
+
+
+class Metadata(BaseModel):
+    metadata: str
+    sha1: str
+
+
+@application.post("/upload/")
+async def upload(request: Request, metadata: UploadFile, raw: UploadFile):
+    print(request.method, request.url.path)
+    metadata_ = Metadata(**json.load(metadata.file))
+
+    started = time.monotonic()
+
+    contents = await raw.read()
+    sha1 = hashlib.sha1(contents).hexdigest()
+
+    elapsed = time.monotonic() - started
+    throughput = humanize.naturalsize(len(contents) / elapsed)
+
+    print(f"{len(contents)=} {sha1=} {elapsed=:.2f}s {throughput=}")
+
+    return {
+        "metadata": metadata_.model_dump(),
+        "filename": raw.filename,
+        "sha1": {"received": metadata_.sha1, "computed": sha1},
+        "match": metadata_.sha1 == sha1,
+        "elapsed": elapsed,
+        "size": len(contents),
+        "throughput": throughput,
+    }
