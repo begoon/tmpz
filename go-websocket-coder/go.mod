b68f0c89b214ed3bb9dad8b3a32a61a0f9bd79d6
Alexander Demin <ademin@gmail.com>
2025-02-24 14:33:33 +0100

    Add websocket monitor

--- go-websocket-coder/go.mod
+++ go-websocket-coder/go.mod
@@ -2,4 +2,9 @@ module ws-coder
 
 go 1.24.0
 
-require github.com/coder/websocket v1.8.12 // indirect
+require github.com/coder/websocket v1.8.12
+
+require (
+	github.com/go-resty/resty/v2 v2.16.5 // indirect
+	golang.org/x/net v0.33.0 // indirect
+)
--- go-websocket-coder/main.go
+++ go-websocket-coder/main.go
@@ -1,36 +1,85 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"errors"
 	"fmt"
+	"html/template"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/coder/websocket"
+	"github.com/go-resty/resty/v2"
 )
 
-var trace strings.Builder
+type tracer struct {
+	data     strings.Builder
+	Monitors sync.Map
+}
+
+func (w *tracer) Write(p []byte) (n int, err error) {
+	m := sync.Mutex{}
+	m.Lock()
+	defer m.Unlock()
+
+	w.data.Write(p)
+	w.Monitors.Range(func(_, v any) bool {
+		ch := v.(chan string)
+		ch <- strings.TrimSpace(string(p))
+		return true
+	})
+	return len(p), nil
+}
+
+func (w *tracer) String() string {
+	m := sync.Mutex{}
+	m.Lock()
+	defer m.Unlock()
+
+	return w.data.String()
+}
+
+type Logger struct {
+	l *log.Logger
+}
+
+func (l Logger) Errorf(format string, v ...interface{}) {
+	l.l.Printf(format, v...)
+}
+
+func (l Logger) Debugf(format string, v ...interface{}) {
+	l.l.Printf(format, v...)
+}
+
+func (l Logger) Warnf(format string, v ...interface{}) {
+	l.l.Printf(format, v...)
+}
 
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
 	}
+	tracer := tracer{Monitors: sync.Map{}}
+
 	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain")
-		lines := strings.Split(trace.String(), "\n")
-		for i := len(lines) - 1; i >= 0; i-- {
-			fmt.Fprintln(w, lines[i])
+		data := strings.Split(tracer.String(), "\n")
+		for i := len(data) - 1; i >= 0; i-- {
+			fmt.Fprintln(w, data[i])
 		}
 	})
+
 	http.HandleFunc("GET /ws/{id}", func(w http.ResponseWriter, r *http.Request) {
 		id := r.PathValue("id")
 
-		tw := io.MultiWriter(&trace, os.Stdout)
+		tw := io.MultiWriter(os.Stdout, &tracer)
 		l := log.New(tw, id+" | ", log.LstdFlags|log.Lmsgprefix)
 
 		c, err := websocket.Accept(w, r, nil)
@@ -40,7 +89,7 @@ func main() {
 		}
 		defer c.CloseNow()
 
-		ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*600)
 		defer cancel()
 
 		l.Printf("connected")
@@ -49,7 +98,12 @@ func main() {
 		for {
 			t, b, err := c.Read(ctx)
 			if err != nil {
-				l.Printf("read: %v", err)
+				var ce websocket.CloseError
+				if errors.As(err, &ce) {
+					l.Printf("read: %d %v", ce.Code, ce.Reason)
+				} else {
+					l.Printf("read: %v", err)
+				}
 				cerr = err
 				break
 			}
@@ -60,7 +114,7 @@ func main() {
 				text = fmt.Sprintf("%v", b)
 			}
 			l.Printf("received %s (%d): %v", t, len(b), text)
-			if t == websocket.MessageText && b[0] == '.' {
+			if t == websocket.MessageText && len(b) > 0 && b[0] == '.' {
 				reason := strings.TrimSpace(string(b[1:]))
 				cerr = c.Close(websocket.StatusNormalClosure, reason)
 				break
@@ -84,9 +138,92 @@ func main() {
 		l.Println("disconnected", code)
 	})
 
-	tw := io.MultiWriter(&trace, os.Stdout)
+	http.HandleFunc("GET /monitor", func(w http.ResponseWriter, r *http.Request) {
+		c, err := websocket.Accept(w, r, nil)
+		if err != nil {
+			msg := fmt.Sprintf("websocket handshake: %v", err)
+			http.Error(w, msg, http.StatusBadRequest)
+			return
+		}
+		defer c.CloseNow()
+
+		ch := make(chan string)
+		id := time.Now().Format(time.RFC3339)
+		tracer.Monitors.Store(id, ch)
+		defer tracer.Monitors.Delete(id)
+
+		ctx := r.Context()
+		log.Printf("monitor/connected")
+		go func() {
+			for {
+				if _, _, err := c.Read(ctx); err != nil {
+					close(ch)
+					return
+				}
+			}
+		}()
+
+		tmpl := `<div id="monitor" hx-swap-oob="afterbegin"><pre>{{.}}</pre></div>`
+		t := template.Must(template.New("monitor").Parse(tmpl))
+		for m := range ch {
+			data := bytes.NewBuffer(nil)
+			err := t.Execute(data, m)
+			if err != nil {
+				log.Printf("monitor/template: %v", err)
+				return
+			}
+			err = c.Write(ctx, websocket.MessageText, data.Bytes())
+			if err != nil {
+				log.Printf("monitor/write: %v", err)
+				return
+			}
+		}
+		log.Printf("monitor/disconnected")
+	})
+
+	http.HandleFunc("GET /live", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		fmt.Fprint(w, monitorHTML)
+	})
+
+	http.HandleFunc("GET /ip", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+
+		tw := io.MultiWriter(os.Stdout, &tracer)
+		l := log.New(tw, "ip | ", log.LstdFlags|log.Lmsgprefix)
+
+		req := resty.New().R().
+			SetContext(r.Context()).
+			SetDebug(true).SetLogger(Logger{l})
+		resp, err := req.Get("https://api.ipify.org")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		fmt.Fprintln(w, resp.String())
+	})
+
+	tw := io.MultiWriter(&tracer, os.Stdout)
 	l := log.New(tw, "", log.LstdFlags|log.Lmsgprefix)
 
 	l.Printf("listening on %s", port)
 	l.Fatal(http.ListenAndServe(":"+port, nil))
 }
+
+const monitorHTML = `<!DOCTYPE html>
+<html>
+<head>
+    <script src="https://unpkg.com/htmx.org@2.0.4"></script>
+    <script src="https://unpkg.com/htmx-ext-ws@2.0.2"></script>
+	<meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+</head>
+<div hx-ext="ws" ws-connect="/monitor"></div>
+<div id="monitor" class="text-[0.7em]"></div>
+<button hx-get="/ip" class="fixed right-0 top-0 p-2 bg-blue-500 text-white rounded">
+  IP
+</button>
+</body>
+</html>
+`
