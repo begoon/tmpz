a447b416508f293801b146c2709ad11600461dd2
Alexander Demin <alexander@demin.ws>
2025-04-07 16:16:10 +0100

    add more to tview

--- go-tcell/tview/main.go
+++ go-tcell/tview/main.go
@@ -1,12 +1,47 @@
 package main
 
 import (
+	"log"
+
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
 func main() {
-	box := tview.NewBox().SetBorder(true).SetTitle("| hello, world! |")
-	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
-		panic(err)
+	app := tview.NewApplication()
+
+	box := tview.NewBox()
+	modal := tview.NewModal().
+		SetText("Do you want to quit the application?").
+		AddButtons([]string{"Quit", "Cancel"}).
+		SetDoneFunc(func(buttonIndex int, buttonLabel string) {
+			if buttonLabel == "Quit" {
+				app.Stop()
+			}
+			if buttonLabel == "Cancel" {
+				app.SetRoot(box, true)
+			}
+		}).SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		switch event.Key() {
+		case tcell.KeyEscape:
+			app.SetRoot(box, true)
+		}
+		return event
+	})
+
+	box.SetBorder(true).SetTitle("| commanger |").
+		SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+			switch event.Key() {
+			case tcell.KeyCtrlC, tcell.KeyEscape:
+				app.SetRoot(modal, false)
+			}
+			switch event.Rune() {
+			case 'q':
+				app.SetRoot(modal, true)
+			}
+			return event
+		})
+	if err := app.SetRoot(box, true).Run(); err != nil {
+		log.Fatalf("start application: %v", err)
 	}
 }
