232772ecc4524dd1e5749cc2fda6d61fd64a9bf3
Alexander Demin <alexander@demin.ws>
2025-05-19 15:16:37 +0100

    add swift-playground

---
+++ swift/Playground.playground/Contents.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+let greeting = "Ha?"
+print(type(of: greeting), Thread.current)
+
+actor Counter {
+    var x: Int = 0
+
+    func increment() {
+        x += 1
+        print("$", x, terminator: " -> ")
+    }
+}
+
+var counter = Counter()
+
+Task {
+    for i in 0 ..< 10 {
+        print("@", i, await Task.detached {
+            await counter.increment()
+        }.value)
+    }
+    await print("x", counter.x)
+}
