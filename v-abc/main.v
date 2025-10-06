97de298d552b48d6255e70dd19524e3a18424fc8
Alexander Demin <alexander@demin.ws>
2025-10-06 13:50:00 +0100

    more v

--- v-abc/main.v
+++ v-abc/main.v
@@ -1,11 +1,20 @@
+struct Type {
+	a int
+	b string
+}
+
 fn main() {
-	a := 69
+	v := Type{
+		a: 123
+		b: 'hello'
+	}
+	println(v)
+	a := [1, 2, 3, 4, 5]
 	f := fn [a] () string {
 		println('inside f() | a = ${a}')
 		return '123'
 	}
-	f()
-	println('hello [${f()}], V!')
+	println('abc [${f()}], V!')
 	println(xx())
 	yy := xx() + '|' + xx() + '|' + int(10).str()
 	println(yy)
