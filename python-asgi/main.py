6d144f443f45b87a93afabe661aae9b0e950675b
Alexander Demin <alexander@demin.ws>
2025-12-27 10:30:16 +0000

    update python-asgi

--- python-asgi/main.py
+++ python-asgi/main.py
@@ -2,35 +2,35 @@ import asyncio
 
 
 async def app(scope, receive, send):
-    assert scope['type'] == 'http'
+    assert scope["type"] == "http"
     print(scope)
-    client = str(scope['client'])
+    client = str(scope["client"])
     await send(
         {
-            'type': 'http.response.start',
-            'status': 200,
-            'headers': [
-                [b'content-type', b'text/plain'],
-                [b'X-Content-Type-Options', b'nosniff'],
+            "type": "http.response.start",
+            "status": 200,
+            "headers": [
+                [b"content-type", b"text/plain"],
+                [b"X-Content-Type-Options", b"nosniff"],
             ],
         }
     )
-    chunks: list[str] = ['dial', 'from', client, '!']
+    chunks: list[str] = ["dial", "from", client, "!"]
     for chunk in chunks:
-        chunk = chunk + '\n'
+        chunk = chunk + "\n"
         await send(
             {
-                'type': 'http.response.body',
-                'body': chunk.encode(),
-                'more_body': True,
+                "type": "http.response.body",
+                "body": chunk.encode(),
+                "more_body": True,
             }
         )
-        print('sent chunk:', chunk.strip())
+        print("sent chunk:", chunk.strip())
         await asyncio.sleep(1)
-    await send({'type': 'http.response.body', 'body': b''})
+    await send({"type": "http.response.body", "body": b""})
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(app)
